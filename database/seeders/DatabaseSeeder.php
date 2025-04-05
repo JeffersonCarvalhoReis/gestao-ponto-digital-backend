40db19290b269ed1bf7cb32fe109be117c50fe3d
Jefferon Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-04-05 04:17:21 -0300

    mudança na rota padrao

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Secrets\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,16 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        if (file_exists(base_path('private/UserSeeder.php'))) {
+            require_once base_path('private/UserSeeder.php');
+            $this->call([
+                PermissionsSeeder::class,
+                RolesSeeder::class,
+                SetorSeeder::class,
+                LocalidadesSeeder::class,
+                UnidadesSeeder::class,
+                UserSeeder::class,
+            ]);
+        } else {
+            $this->call([
+                PermissionsSeeder::class,
+                RolesSeeder::class,
+                SetorSeeder::class,
+                LocalidadesSeeder::class,
+                CargosSeeder::class,
+                UnidadesSeeder::class,
+                FuncionarioSeeder::class,
+                UsersSeeder::class,
+            ]);
+        }
 
-        $this->call([
-            PermissionsSeeder::class,
-            RolesSeeder::class,
-            SetorSeeder::class,
-            LocalidadesSeeder::class,
-            CargosSeeder::class,
-            UnidadesSeeder::class,
-            FuncionarioSeeder::class,
-            UsersSeeder::class,
-        ]);
+
     }
 }
